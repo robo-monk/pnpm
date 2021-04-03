c5f33ae0463513804536b24353113432fbc81cf9
robo-monk <rrobomonk@gmail.com>
2021-04-04 00:18:52 +0200

    script order

--- copy+paste.py
+++ copy+paste.py
@@ -1,12 +1,13 @@
 import re
 import subprocess as sb
 
-regex = r"Location: ([^ ]+)"
-_path = sb.check_output("pip3 show pnpm", shell=True).decode('utf-8').replace("\n", " ")
 sb.call("pip3 uninstall pnpm -y", shell=True)
 sb.call("pip3 install pnpm --upgrade", shell=True)
+
+regex = r"Location: ([^ ]+)"
+_path = sb.check_output("pip3 show pnpm", shell=True).decode('utf-8').replace("\n", " ")
 sb.call("python3 " + re.search(regex, _path).group(1) + "/pnpm/install", shell=True)
 
-def abs_path(sub=''):
-    return _path  + sub
+# def abs_path(sub=''):
+    # return _path  + sub
 
