3eb35d29b93497349b83c85a6f42b092a52bd6df
robo-monk <rrobomonk@gmail.com>
2021-04-03 23:31:21 +0200

    vw

--- pnpm/helpers.py
+++ pnpm/helpers.py
@@ -1,4 +1,5 @@
 import re
+import subprocess as sb
 
 regex = r"Location: ([^ ]+)"
 _path = sb.check_output("pip3 show pnpm", shell=True).decode('utf-8').replace("\n", " ")
@@ -6,3 +7,4 @@ _path = re.search(regex, _path).group(1) + "/pnpm"
 
 def abs_path(sub=''):
     return _path  + sub
+
