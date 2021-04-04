fcf33ef40734b7adca3010f78379a178d9198c40
robo-monk <rrobomonk@gmail.com>
2021-04-04 02:14:35 +0200

    junky cli options

---
+++ docs/.pnpm/scripts/_script.py
@@ -0,0 +1,28 @@
+import subprocess as sb
+import sys
+import json
+from config import config
+import os
+
+_params = json.loads(sys.argv[1]) if len(sys.argv)>1 else {}
+def params(key=None):
+    if key is None:
+        return _params
+
+    if key in _params:
+        return _params[key]
+    return None
+
+
+env = params('env') or "dev"
+pkg_manager = params('use') or config('package_manager')
+
+if params("reload") or env=='prod':
+    sb.call(f"pip3 install -r .pnpm/requirements.txt", shell=True)
+    sb.call(f"{pkg_manager} install", shell=True)
+# sb.call(f"source .docs_env/bin/activate", shell=True)
+
+
+
+def run(sub):
+    sp.call(f"python3 .pnpm/{sub}", shell=True)
